49fa94c13551f178ffae911f4e098690c2e72fe0
luizinhadenatal <luiza.formentin@escola.pr.gov.br>
2025-10-03 11:14:20 -0300

    update

---
+++ exe 3.py
@@ -0,0 +1,25 @@
+loja_frutas = {
+    'maça': {'preço': 3.50,
+              'quantidade':10},
+    'banana': {'preço': 3.50,
+              'quantidade' :10},
+    'uva': {'preço': 3.50,
+            'quantidade':10},
+}
+
+#R:João quer adicionar mais três frutas novas ao seu sistema.
+loja_frutas.update({'limão':{'preços': 3.50,'quantidade':10},
+                   'laranja': {'preço': 3.50, 'quantidade':10},
+                   'abacaxi': {'preço': 3.50, 'quantidade':10}})
+print(loja_frutas)
+
+for k,v in loja_frutas.items()
+    loja_frutas.get(k)['preço']*=1.15
+print(loja_frutas
+
+for k,v in loja_frutas.items()
+    loja_frutas.get(k)['quantidade']*=20
+print(loja_frutas)
+
+loja_frutas.pop('maça')
+
